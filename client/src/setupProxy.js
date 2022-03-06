2b0287647656571afac567c2a9c3b535b85c1113
Keeeparis <vladimir1trotsenko@gmail.com>
2022-03-06 15:56:24 +0600

    add docker files

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:5500',
+            target: 'http://server:5500',
             changeOrigin: true,
         })
     );
